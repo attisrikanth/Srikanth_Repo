1a2e63f08e3f91fae47f268358eb9055734ff23e
attisrikanth <srikanthpspk1996@gmail.com>
2018-08-29 17:22:29 +0530

    first commit Srikanth

---
+++ Srikanth_proj1/src/main/java/Srikanth_group/Srikanth_proj1/App.java
@@ -0,0 +1,13 @@
+package Srikanth_group.Srikanth_proj1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
